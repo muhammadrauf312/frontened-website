005554cd2ee81f3cd293fbb85a8f703227967d27
muhammadrauf312 <129946073+muhammadrauf312@users.noreply.github.com>
2025-10-01 16:00:51 +0500

    Add files via upload

---
+++ reviews.js
@@ -0,0 +1,145 @@
+var card1 =
+[
+ {"Id":0,"Name":"Alex Ben", "Img":"1.jpg","Title":"APPLE IPHONE 14 PRO MAX","Para":"Overall, the iPhone 14 Pro Max with its powerful display, large frame, enhanced pixels and stabilisation and quirky features like the Dynamic Island, makes it worth the fancy upgrade."},
+ {"Id":1,"Name":"Tony Lang","Img":"1.jpg","Title":"APPLE IPHONE 13 PRO MAX","Para":"The iPhone 13 Pro Max's stellar cameras, 120Hz ProMotion display and longer battery life add up to a practically perfect phone in our testing. The charging could be faster and Touch ID would be nice, but overall this is a beast that can't be beat"},
+ {"Id":2,"Name":"Chris Evans","Img":"1.jpg","Title":"APPLE IPHONE 12 PRO MAX","Para":"Is iPhone 12 Pro Max worth buying. If you're a budding movie-maker, we'd say it's definitely worth looking at the 12 Pro Max – it's packing Dolby Vision recording at 4K and 60 frames per second, so if you're a content creator looking for a phone that can record high-quality footage, the 12 Pro Max can definitely do that"}
+ 
+ 
+]
+var card2 =
+[
+ {"Id":0,"Name":"Kevin John","Img":"1.jpg","Title":"APPLE IPHONE 11 PRO MAX ","Para":"f you want the best iPhone features — large OLED display, triple cameras, and more — for less money: Yes, the iPhone 11 Pro Max is definitely worth buying in 2022."},
+ {"Id":1,"Name":"Jos Buttler","Img":"1.jpg","Title":"APPLE IPHONE XS MAX","Para":"I would only recommend the Max version, and always recommend 256 GB. So, based on my parameters, yes, because you still get upgrades fo 5–8 years, and a very capable phone. Both the 11 and 12 are more expensive, and if you want a better camera, buy a real camera."},
+ {"Id":2,"Name":"Zara Olsen","Img":"2.webp","Title":"APPLE IPHONE 8 ","Para":"As you can see from this post, the iPhone 8 and 8 Plus are still worth buying in 2022. But, it really comes down to your expectations. Although they're a far cry from the latest iPhones in terms of features and capabilities, they're also a fraction of the price"}
+ 
+ 
+]
+var card3 =
+[
+ {"Id":0,"Name":"Elisbeth Shin","Img":"2.webp","Title":"VIVO Y33T","Para":"Vivo Y33t is a good budget phone but the phone is a plastic body and I hate it scratching and getting dirty, so I wrapped it with Gadgetshieldz skin and now it is protected and looks so cool! The performance is also good!"},
+ {"Id":1,"Name":"John Cena","Img":"1.jpg","Title":"VIVO Y55","Para":"I purchased this phone model (Y55), and I find it to be good, reliable and it has everything that I want from any mobile 'phone. The purchase price was good. I am happy to recommend this 'phone to anyone who wants a reliable unit, such as this."},
+ {"Id":2,"Name":"Dwyane Johnson","Img":"1.jpg","Title":"VIVO Y21","Para":"I'm buying this phone 2 month ago , in that duration only one month phone is running smooth but after complete of one month phone runs very slow (hanging problem) and also camera result very poor quality . so i suggest all of you don't buy vivoy21 mobile it is waste of money."}
+ 
+ 
+]
+var card4 =
+[
+ {"Id":0,"Name":"Clint Bartan","Img":"1.jpg","Title":"VIVO X80","Para":"The Vivo X80 is, without question, one of the most capable phones you can get under Rs 60,000 – if not the most capable phone. It's incredibly powerful, packed with features, and delivers great performance and top class battery life"},
+{"Id":1,"Name":"Laila Bartan","Img":"2.webp","Title":"VIVO Y02s ","Para":"For a budget phone running on the Helio P35 processor, it runs relatively decently if you're not the kind of consumer that jams their phone chock-full of games. The screen isn't 4K or anything premium, but it's also not too bad and doesn't look like a projector."},
+ {"Id":2,"Name":"May Jos","Img":"2.webp","Title":"VIVO Y23","Para":"The good thing about the device includes a massive battery, an amazing display, a large storage capacity alongside a lot of other great specs as well. The Vivo Y23 will reportedly sport an amazing 6.5 inch (16.54 cm) IPS LCD display tipped with a resolution of 720 x 1600 pixels."}
+ 
+ 
+]
+var card5 =
+[
+ {"Id":0,"Name":"James Joseph","Img":"1.jpg","Title":"OPPO A96","Para":"The OPPO A96 is a mid-range 4G device by OPOP which comes with a 6.59-inch display. The display panel has a Full HD+ resolution and a 90Hz refresh rate. Thanks to Snapdragon 680 SoC and 8GB of RAM, the device offers excellent performance."},
+ {"Id":1,"Name":"Robert Downey","Img":"1.jpg","Title":"OPPO A57","Para":"The OPPO A57 2022 is a pretty good all-around performer. Despite its underpowered processor and lack of FHD+ visuals, the handset makes up with its looks, punchy speakers, amazing battery life coupled with fast charging, and decent camera system. However, if speed is your top priority, you might want to look elsewhere."},
+ {"Id":2,"Name":"Mary","Img":"2.webp","Title":"OPPO A76","Para":"Overall 7.5 / 10. As a daily use phone its a better option out of the available choices. Battery timing is good, Very stable on Network quality, Decent screen controls, Camera is really ordinary like Oppo cant highlight the Camera of this phone as its main or strong feature. Price is slightly on the higher side."}
+ 
+ 
+]
+var card6 =
+[
+    {"Id":0,"Name":"Patricia","Img":"2.webp","Title":"OPPO F21 PR0 5G","Para":"The OPPO F21 Pro 5G is a phone that might not match hardware requirements from a phone that costs above Rs 25,000, but it sure does score on looks. Daylight camera performance is good enough as is the battery life, but the lack of a high refresh display or stereo speakers cuts into the usability of the device."},
+    {"Id":1,"Name":"Michael","Img":"1.jpg","Title":"OPPO RENO 6 Pro","Para":"All of this doesn't change the fact that the Oppo Reno 6 is a smartphone that offers a satisfactory experience – including good camera performance, a high-quality display, ample battery life and fast charging which are all good things to have at this price range."},
+    {"Id":2,"Name":"Ralph","Img":"1.jpg","Title":"OPPO REno 5 Pro","Para":"The Oppo Reno 5 Pro 5G is a good package overall for the price. It has a premium design, a vivid display, long battery life, very fast charging, and fairly competent cameras. Video quality could be improved though, especially in low light, and I would like to see stabilisation at 4K."}
+    
+ 
+]
+var card7 =
+
+[
+  {"Id":0,"Name":"David","Img":"1.jpg","Title":"INFINIX HOT 12","Para":"Final verdict. Infinix has a decent offering on its hand with the Hot 12 Play, which retails at an appealing price of Rs 8,699 in India. The phone is let down by less than ideal image-taking capabilities and a poor user interface."},
+  {"Id":1,"Name":"Jessica","Img":"2.webp","Title":"INFINIX NOTE 12 VIP","Para":"ther than that the phone is perfect. The best display, outstanding battery backup, great gaming performance Great cameras.. overall the phone is fantastic.. one more thing .. it has got 33w fast charger which charges your phones 5000 mAh battery from zero to hundred in just 1 hour 10 minutes."},
+  {"Id":2,"Name":"Thomas","Img":"1.jpg","Title":"INFINIX 6hd","Para":"Phone is good and for entry level android users . battery life good. for this price give better screen and all necessary sensors . Good performance for back camera and front camera."}
+    
+ 
+]
+var card8 =
+[
+    {"Id":0,"Name":"Scott","Img":"1.jpg","Title":"INFINIX ZERO 5G","Para":"The Infinix Zero 5G keeps up with appearances thanks to a nearly flawless design and smooth performance. Unfortunately, for the price of Rs 19,999, the device is about average in all other aspects such as display and camera quality while its software implementation isn't too great either."},
+    {"Id":1,"Name":"Charles","Img":"1.jpg","Title":"INFINIX ZERO 20 ","Para":"The ZERO 20 is equipped with an industry-first 60MP OIS front camera for top-notch vlogging capabilities.The ZERO 20 is powered by the MediaTek Helio G99 for a high-performance monster engine – enabling the next generation of gaming."},
+    {"Id":2,"Name":"Daniel","Img":"1.jpg","Title":"INFINIX ZERO X NEO","Para":"Some aspects of the Zero X Pro are hard to ignore and deserve praise, like the eye-catching design and solid build, sharp and snappy AMOLED panel, with surprisingly competent 120Hz handling. In fact, XOS 7.6 is actually a very fluid and highly feature-dense Android."}
+    
+ 
+]
+var card9 =
+[
+ {"Id":0,"Name":"Mark","Img":"1.jpg","Title":"REALME 9i","Para":"The Realme 9i is not an extraordinary phone but delivers on the basics, offering decent daylight photography, good battery life, a capable screen and rich-sounding stereo speakers."},
+ {"Id":1,"Name":"Steven","Img":"1.jpg","Title":"REALME 9","Para":"The Redmi 9 has a quad-camera setup, which is a big improvement over the Redmi 8. The main sensor is 13MP at f/2.2, it's supported by an 8MP ultra-wide angle sensor, a 5MP macro sensor, and a 2MP depth sensor. Overall, the camera is above average when you consider the price point."},
+ {"Id":2,"Name":"Andrew","Img":"1.jpg","Title":"REALME GT","Para":"The Realme GT Master Edition certainly offers good value for money, given that you get a new Snapdragon 778G processor, a high-quality Super AMOLED display with a 120Hz refresh rate, 65W charging, and good battery life."}
+ 
+ 
+]
+var card10 =
+[
+ {"Id":0,"Name":"George","Img":"1.jpg","Title":"REALME 7 PRO","Para":"The Realme 7 Pro offers great value for its price. Highpoints include its AMOLED display, awesome dart charging convenience, and the elegant, ergonomic design. The primary 64MP camera performance is above average and comparable to the best available options under 20k, but the overall camera performance is a mixed bag."},
+ {"Id":1,"Name":"Ronald","Img":"1.jpg","Title":"REALME NAZRO 30","Para":"If you are on a tight budget and 5G isn't a priority, then the Realme Narzo 30 (prices starting at Rs 12,499) is a good budget smartphone that offers a great everyday usage experience, mid-level gaming performance, decent photos and videos in daylight, and good battery life."},
+ {"Id":2,"Name":"Angela","Img":"2.webp","Title":"REALME 8","Para":"The Realme 8 is a solid, if unremarkable, budget buy. For the price you're getting a dependable main camera, strong battery life, and OLED screen, all backed up by a smooth software experience that prioritises ease-of-use"}
+ 
+ 
+]
+var card11 =
+[
+ {"Id":0,"Name":"Ryan","Img":"1.jpg","Title":"SAMSUNG GLAXY A33","Para":"With its Galaxy A33 5G, Samsung has identified and remedied the weaknesses of the Galaxy A32 5G predecessor. With a 90 Hz AMOLED display, a more modern SoC and a waterproof case, the mid-range smartphone even comes dangerously close to matching the Galaxy A53 5G."},
+ {"Id":1,"Name":"Stephen","Img":"1.jpg","Title":"SAMSUNG GALAXY S21 FE 256GB","Para":"The iPhone 14 Pro Max isn’t just the most powerful phone around. It’s a joy to use, thanks to the clever new Dynamic Island for displaying notifications and live activities."},
+ {"Id":2,"Name":"Brandon","Img":"1.jpg","Title":"SAMSUNG Galaxy Z Flip 4","Para":"The Galaxy Z Flip 4 is really special, and it's the first Samsung foldable that I've started recommending to anyone who asks if it's worth it. Again, it's not a phone for anyone who wants a full-featured flagship camera experience, but for anyone else, it's excellent."}
+ 
+ 
+]
+var card12 =
+[
+ {"Id":0,"Name":"Justin","Img":"1.jpg","Title":"SAMSUNG GALAXY S21 FE","Para":"Is the Samsung Galaxy S21 FE worth buying? On paper, the Galaxy S21 FE is a great phone. For under $700, you get one of the best mobile chipsets of 2021, a decent camera system, Android 12 out-of-the-box, and more. No matter how you shake it, that's not bad."},
+ {"Id":1,"Name":"Alexander","Img":"1.jpg","Title":"SAMSUNG GALAXY NOTE 20","Para":"So is the Note 20 Ultra worth the ultra-premium price? After thoroughly testing this big-screen phone, my previous answer was yes. However, most should wait for the Samsung Galaxy S22 Ultra, which is rumored to include a built-in stylus and could be a proper replacement for the Galaxy Note."},
+ {"Id":2,"Name":"Jerry","Img":"1.jpg","Title":"SAMSUNG GALAXY S10 LITE","Para":"The screen is crazy good indoors, decent outdoors, camera is midrange level, soc is amazing at prolonged use, and the glasstic feels so good in hand, no scratches after 2 years naked."}
+ 
+ 
+]
+var card13 =
+[
+ {"Id":0,"Name":"Henry","Img":"1.jpg","Title":"XIAOMI REDME NOTE 11 6GB","Para":"The Redmi Note 11, like any budget phone, is a compromise: the design, build quality, screen and battery life are great, but the rear camera quality and the overall performance are where you'll notice the savings on the price of the handset have been made."},
+ {"Id":1,"Name":"Peter","Img":"1.jpg","Title":"XIAOMI POCO M4 PRO 8GB","Para":"Don't be misled by its 90hz refresh rate.” “From my experience after four months of usage, the Poco M4 Pro 5G is a good smartphone. It has a large and fluid screen, stereo speakers, okay performance, capable cameras, all-around connectivity with 5G, and long-lasting battery life with fast charging,”."},
+ {"Id":2,"Name":"Roger","Img":"1.jpg","Title":"XIAOMI REDMI 10C 128GB","Para":"this is a very stupid device not a smartphone.. Definitely one of the best devices in this price range. There are a few flaws but what a processor, great main sensor camera and awesome battery life. Definitely best value for money at this price point."}
+ 
+ 
+]
+var card14 =
+[
+ {"Id":0,"Name":"Austin","Img":"1.jpg","Title":"XIAOMI 12 PRO","Para":"The Xiaomi 12 Pro packs plenty of raw power for blazing-fast performance and charging speeds, but falls short in vital areas such as the battery and the camera. A good enough buy for those with a need for speed, but you can get truly great phones for the same price or less"},
+ {"Id":1,"Name":"Carl","Img":"1.jpg","Title":"XIAOMI POCO M4 PRO 5G","Para":"The main camera on the Poco M4 5G is decent in low-light conditions, especially for a budget phone. There is plenty of detail in the shots, and the color saturation looks good. The only issue here is that noise levels are a bit high. The M4 5G struggles a bit with dark areas and light sources"},
+ {"Id":2,"Name":"Jayne","Img":"2.webp","Title":"XIAOMI POCO X3 PRO 8GB","Para":"The powerful chipset is further paired with up to 8GB RAM and 128GB of fast UFS 3.1 storage. If you are looking for an affordable phone for gaming, the Poco X3 Pro should be your best bet."}
+ 
+ 
+]
+var card15 =
+[
+ {"Id":0,"Name":"Bruce","Img":"1.jpg","Title":"NOKIA X20","Para":"The Nokia X20 has a quad-camera arrangement, which consists of a primary 64MP unit (captures 16MP images by default), a 5MP ultrawide and dual 2MP depth and macro sensors. That's pretty much par for the course in this day and age, and the Nokia's 32MP front-facing camera isn't anything out of the ordinary."},
+ {"Id":1,"Name":"Alan","Img":"1.jpg","Title":"NOKIA C21","Para":"As someone else said, the camera is very poor unless outside in sunshine. GPS is so bad it renders Google maps useless for navigating. The phone seems to be sleeping all the time, when you want to do something you have a delay as it wakes up. Battery life is good but not exceptional. My old Motorola G4 play was a faster phone, camera in a different league Nokia c21 plus is the most disappointing and frustrating phone I have ever used."},
+ {"Id":2,"Name":"Randy","Img":"1.jpg","Title":"NOKIA G20","Para":"The Nokia G20 has a genuinely impressive battery that barely drains when idle and should remain functional for days. Its satisfyingly simple stock Android software, striking design, and decent main camera all add up to a more-than-respectable package for a $200 phone."}
+ 
+ 
+]
+var card16 =
+[
+ {"Id":0,"Name":"Philip","Img":"1.jpg","Title":"NOKIA X10","Para":"One of the best value for money phones around. All going well so far. Great phone, reliable fingerprint recognition."},
+ {"Id":1,"Name":"Louis","Img":"1.jpg","Title":"NOKIA 5.4","Para":"The Nokia 5.4 performs well, is durable, and has notably good audio for a budget phone, but the lackluster cameras, average battery life, and lack of 5G are hard to swallow at $250."},
+ {"Id":2,"Name":"Bobby","Img":"1.jpg","Title":"NOKIA 3.4","Para":"The Nokia 3.4 is a alright phone - fast enough for daily usage, stable(ish), mostly stock Android - but the camera is the worst I have ever seen on a new phone, budget or not."}
+ 
+ 
+]
+var card17 =
+[
+ {"Id":0,"Name":"Russell","Img":"1.jpg","Title":"TECHNO SPARK 8C","Para":"The Tecno Spark 8C is one of the best budget smartphones you can buy right now. It comes with 4GB or 6GB RAM and it cost less than $150 I know you are surprised with the RAM I'll explain more in a bit."},
+ {"Id":1,"Name":"Roy","Img":"1.jpg","Title":"TECHNO POVA NEO","Para":" The Tecno POVA is definitely a strong contender in the budget segment. In its own right, it has great features that potential buyers would love - decent camera, big screen real estate, and gaming-oriented processor"},
+ {"Id":2,"Name":"Willie","Img":"1.jpg","Title":"TECHNO POP 5 LTE ","Para":"TECNO Pop 5  LTE is sluggish.This is the main downside to the phone: it is sluggish. The combination of the cheap processor with 1GB of RAM and TECNO's custom user interface, HiOS, is a bit too much for the poor thing."}
+ 
+ 
+]
+var card18 =
+[
+ {"Id":0,"Name":"Albert","Img":"1.jpg","Title":"TECNO CAMON 19 PRO ","Para":"The Camon 19 Pro has plenty going for it beyond what we already mentioned. Its large 6.8-inch display has a 120Hz resolution. Though, unlike the Camon 18 Premier, it's just an LCD rather than an OLED. You also get a large 5,000 mAh battery with 33W fast charging and a decently-powerful MediaTek Helio G96 chipset"},
+ {"Id":1,"Name":"Jordan","Img":"1.jpg","Title":"TECNO CAMON 18P","Para":"Tecno Camon 18 is a budget smartphone that comes with a couple of sacrifices, but it also offers many advantages. A stunning screen, a quick fingerprint scanner, a great set of cameras, and impressive autonomous performance are just a few of them."},
+ {"Id":2,"Name":"Lawrence","Img":"1.jpg","Title":"TECNO CAMON 19 PRO MONDRIANS","Para":"Expensive set but still lack many features ie old Android version, low performance chip set, No - NFC, Infrared, Compass, Gyroscope etc. Seems to be failed in Pak market."}
+ 
+ 
+]
